2f56b84c75b8062e4d8006f0e13ca7b11b7a6c42
sanad <you@example.com>
2023-09-16 16:51:10 +0300

    fix

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-// Print given text to console
-const myVar = 'JavaScript is amazing';
+// Print string “Javascript is amazing”
+
+const myVar = 'Javascript is amazing';
 console.log(myVar);
